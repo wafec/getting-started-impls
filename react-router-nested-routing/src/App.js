d2ac1d96bb6ce7ca20c7efdc713cf06e39a9d647
Cardoso <Wallace.Cardoso@philips.com>
2021-08-25 16:06:26 -0300

    react router dom + react lazy load completed

---
+++ react-router-nested-routing/src/App.js
@@ -0,0 +1,48 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Topics = React.lazy(() => import("./Topics"));
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/topics">
+            <Suspense fallback={<div>Loading...</div>}>
+              <Topics />
+            </Suspense>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+export default App;
