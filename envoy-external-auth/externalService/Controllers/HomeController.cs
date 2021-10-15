cbd9bbbc96894d7551595379f7293aa6010c7a72
Wallace <wallacefcardoso@gmail.com>
2021-10-15 16:49:24 -0300

    add envoy-external-auth inprogress

---
+++ envoy-external-auth/externalService/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace externalService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Console.WriteLine("Home Get Called");
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            Console.WriteLine("Home Post Called");
+            return Ok();
+        }
+    }
+}
